2355f206f9c973df0a8a30c1e07e522057fe9136
Aaron <aaron@Aarons-MacBook-Pro.local>
2019-07-27 23:47:35 +0530

    some commit

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -10,78 +10,59 @@ class Dashboard extends Component {
         this.state = {
             formData: {}, // Contains login form data
             errors: {}, // Contains login field errors
-            formSubmitted: false, // Indicates submit status of login form 
             loading: false // Indicates in progress state of login form
         }
     }
 
-    handleInputChange = (event) => {
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-
-        let { formData } = this.state;
-        formData[name] = value;
-
-        this.setState({
-            formData: formData
-        });
-    }
-
-    validateLoginForm = (e) => {
-        
-        let errors = {};
-        const { formData } = this.state;
-
-        //if (isEmpty(formData.email)) {
-        //    errors.email = "Email can't be blank";
-        //} else if (!isEmail(formData.email)) {
-        //    errors.email = "Please enter a valid email";
-        //}
-
-        //if (isEmpty(formData.password)) {
-        //    errors.password = "Password can't be blank";
-        //}  else if (isContainWhiteSpace(formData.password)) {
-        //    errors.password = "Password should not contain white spaces";
-        //} else if (!isLength(formData.password, { gte: 6, lte: 16, trim: true })) {
-        //    errors.password = "Password's length must between 6 to 16";
-        //}
-
-        //if (isEmpty(errors)) {
-            return true;
-        //} else {
-        //    return errors;
-        //}   
-    }
-
-    login = (e) => {
-        
-        e.preventDefault();
-
-        let errors = this.validateLoginForm();
-
-        if(errors === true){
-            alert("You are successfully signed in...");
-            window.location.reload()   
-        } else {
-            this.setState({
-                errors: errors,
-                formSubmitted: true
-            });
-        }
-    }
 
     render() {
 
-        const { errors, formSubmitted } = this.state;
+        //const { errors, formSubmitted } = this.state;
 
         return (
-            <div className="Login">
+            <div className="Dashboard">
                 <Row>
-                    <form onSubmit={this.login}>
+                    
                             <ControlLabel>Welcome</ControlLabel>
-                    </form>
+                    
                 </Row>
+                <Row>
+                    User
+                </Row><br /> <br />
+                <Row>
+                    <p>
+                        Search over name-of-product :
+                    </p>
+                <input type="text" value="wind" onChange={(e) =>this.handleChange(e.target.value)} />
+
+                </Row><br /> <br />
+                <ul>
+                    <li>
+                        VS-Studio-Pro
+                    </li>
+                    <li>
+                    Windows XP
+                    </li>
+                    <li>
+                    Windows 10
+                    </li>
+                    <li>
+                    Norton Antivirus
+                    </li>
+                    <li>
+                    AWS Account
+                    </li>
+
+                </ul>
+                <br /> <br /> <br />
+                <Row>
+                    <p>
+                        Get Products for name :
+                    </p>
+                <input type="text" value="nishanth" onChange={(e) =>this.handleChange(e.target.value)} />
+
+                </Row><br /> <br />
+
             </div>
         )
     }
